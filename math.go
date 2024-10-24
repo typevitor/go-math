f1d41c2f6ffae4e7de4ac74a703399dc0681c4c2
Vitor Leal <vitorlealeng@gmail.com>
2024-10-24 08:37:01 -0300

    feat: add remaning operations

--- math.go
+++ math.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 func main() {
 	fmt.Println(Sum(10,10))
@@ -8,4 +11,20 @@ func main() {
 
 func Sum(a int, b int) int {
 	return a + b
+}
+
+func Sub(a int, b int) int {
+	return a - b
+}
+
+func Times(a int, b int) int {
+	return a * b
+}
+
+func Div(a int, b int) (float32, error) {
+	if b == 0 {
+		return 0, errors.New("Divisor não pode ser zero")
+	}
+
+	return float32(a) / float32(b), nil
 }
--- math_test.go
+++ math_test.go
@@ -8,3 +8,32 @@ func TestSum(t *testing.T) {
 		t.Errorf("Resultado da soma é inválido: Resuldado: %d. Esperado era: %d", total, 30)
 	}
 }
+
+func TestSub(t *testing.T) {
+	total := Sub(15, 10)
+	if total != 5 {
+		t.Errorf("Resultado da subtração é inválido: Resuldado: %d. Esperado era: %d", total, 5)
+	}
+}
+
+func TestTimes(t *testing.T) {
+	total := Times(15, 2)
+	if total != 30 {
+		t.Errorf("Resultado da multiplicação é inválido: Resuldado: %d. Esperado era: %d", total, 30)
+	}
+}
+
+func TestDiv(t *testing.T) {
+	total,err := Div(15, 3)
+	if total != 5 {
+		t.Errorf("Resultado da divisão é inválido: Resuldado: %.2f. Esperado era: %.2f", total, 5.00)
+	}
+	if err != nil {
+		t.Errorf("Erro não deveria ser retornado");
+	}
+
+	total,err = Div(10, 0)
+	if err == nil {
+		t.Errorf("Divisor não pode ser nulo.");
+	}
+}
