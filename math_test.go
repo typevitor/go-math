1d5c6a7a9043080648f32cf9b18b1ab1fc584a3f
Vitor Leal <vitorlealeng@gmail.com>
2024-10-25 10:04:18 -0300

    feat: remove tests to test sonarcloud

--- math_test.go
+++ math_test.go
@@ -9,31 +9,31 @@ func TestSum(t *testing.T) {
 	}
 }
 
-func TestSub(t *testing.T) {
-	total := Sub(15, 10)
-	if total != 5 {
-		t.Errorf("Resultado da subtração é inválido: Resuldado: %d. Esperado era: %d", total, 5)
-	}
-}
-
-func TestTimes(t *testing.T) {
-	total := Times(15, 2)
-	if total != 30 {
-		t.Errorf("Resultado da multiplicação é inválido: Resuldado: %d. Esperado era: %d", total, 30)
-	}
-}
-
-func TestDiv(t *testing.T) {
-	total,err := Div(15, 3)
-	if total != 5 {
-		t.Errorf("Resultado da divisão é inválido: Resuldado: %.2f. Esperado era: %.2f", total, 5.00)
-	}
-	if err != nil {
-		t.Errorf("Erro não deveria ser retornado");
-	}
-
-	total,err = Div(10, 0)
-	if err == nil {
-		t.Errorf("Divisor não pode ser nulo.");
-	}
+//func TestSub(t *testing.T) {
+//	total := Sub(15, 10)
+//	if total != 5 {
+//		t.Errorf("Resultado da subtração é inválido: Resuldado: %d. Esperado era: %d", total, 5)
+//	}
+//}
+//
+//func TestTimes(t *testing.T) {
+//	total := Times(15, 2)
+//	if total != 30 {
+//		t.Errorf("Resultado da multiplicação é inválido: Resuldado: %d. Esperado era: %d", total, 30)
+//	}
+//}
+//
+//func TestDiv(t *testing.T) {
+//	total,err := Div(15, 3)
+//	if total != 5 {
+//		t.Errorf("Resultado da divisão é inválido: Resuldado: %.2f. Esperado era: %.2f", total, 5.00)
+//	}
+//	if err != nil {
+//		t.Errorf("Erro não deveria ser retornado");
+//	}
+//
+//	total,err = Div(10, 0)
+//	if err == nil {
+//		t.Errorf("Divisor não pode ser nulo.");
+//	}
 }
